325fd046901f9c632aac58cfa8de4c1b8bbe968b
BottyIvan <testbottyivan@gmail.com>
2015-10-09 17:32:23 +0200

    added themed ActionBar

--- app/src/main/java/com/botty/ubuntufontdemo/MainActivity.java
+++ app/src/main/java/com/botty/ubuntufontdemo/MainActivity.java
@@ -1,22 +1,39 @@
 package com.botty.ubuntufontdemo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mikepenz.aboutlibraries.Libs;
 import com.mikepenz.aboutlibraries.LibsBuilder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getActionBar().setDisplayShowCustomEnabled(true);
+        this.getActionBar().setDisplayShowTitleEnabled(false);
+
+        LayoutInflater inflator = LayoutInflater.from(this);
+        View v = inflator.inflate(R.layout.toolbar, null);
+
+        //if you need to customize anything else about the text, do it here.
+        //I'm using a custom TextView with a custom font in my layout xml so all I need to do is set title
+        ((TextView)v.findViewById(R.id.title)).setText(this.getTitle());
+
+        //assign the view to the actionbar
+        this.getActionBar().setCustomView(v);
+
     }
 
     @Override
